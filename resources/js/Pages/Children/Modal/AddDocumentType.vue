c4d3e90a90fb06d09bb1bf1f42c113e62eabcda5
soybean15 <marlonpadilla1593@gmail.com>
2023-11-22 20:11:56 +0800

    add document modal

--- resources/js/Pages/Children/Modal/AddDocumentType.vue
+++ resources/js/Pages/Children/Modal/AddDocumentType.vue
@@ -1,14 +1,29 @@
 <template>
   <slot :open="open"></slot>
   <q-dialog v-model="dialog" persistent>
-    <q-card style="width: 900px; max-width: 80vw">
+    <q-card style="width: 400px; max-width: 80vw">
       <q-card-section class="row items-center q-pb-none">
         <div>Add Document Type </div>
         <q-space />
         <q-btn icon="close" flat round dense v-close-popup />
       </q-card-section>
 
-      <q-card-section class="q-pt-none"> </q-card-section>
+      <q-card-section class="p-5">
+        <q-form>
+
+            <div>
+                <q-input dense outlined label="Name"/>
+            </div>
+
+            <div class="row justify-end py-1">
+                <q-btn type="submit" color="secondary" label="Submit"/>
+
+            </div>
+
+
+        </q-form>
+        
+    </q-card-section>
     </q-card>
   </q-dialog>
 </template>
